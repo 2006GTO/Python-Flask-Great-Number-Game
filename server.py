2d08c060a38c9ec6f77b081cd6cca41945879d50
Tom Reese III <tr3limited@gmail.com>
2018-09-07 17:23:37 -0500

    Add files via upload
    Email validation tester for Python
    
    Install flask using pip in your terminal (pip install Flask)
    
    Install/Run a virtual environment https://uoa-eresearch.github.io/eresearch-cookbook/recipe/2014/11/26/python-virtual-env/
    
    In the root folder of this project, in your terminal, run "python server.py"
    
    Navigate to localhost:5000
    
    Enter your guess of the randomly-generated number.  There is a validation to check if the string entered is a whole integer or not.

---
+++ server.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, redirect, session, render_template
+import random
+app=Flask(__name__)
+app.secret_key='123'
+
+@app.route('/')
+def index():
+    reset=""
+    # test=session['box_type']
+    if 'result' not in session:
+        session['result'] = ""
+    if 'display' not in session:
+        session['display'] = ""
+        print(session['display'])
+    if 'hide' not in session:
+        session['hide'] = "display:none"
+    if 'number' not in session:
+        result = ""
+        session['number'] = str(random.randrange(0,101))
+        print("the number is",session['number'])
+    else:
+        result = session['result']
+    if 'style' not in session:
+        session['style'] = ""
+    print(session['display'])
+    
+    return render_template('index.html',result=result, reset=reset,style=session['style'], display=session['display'], hide=session['hide'])
+
+@app.route('/guess',methods=['POST'])
+def guess():
+    foo = request.form['guess']
+    if(foo.isnumeric()):
+        session['guess'] = int(request.form['guess'])
+        session['number'] = int(session['number'])
+        if session['guess'] == session['number']:
+            print("user guessed the correct number")
+            session['style'] = "nailedit"
+            session['result'] = 'Great job!  You nailed it!'
+            session['display'] = "display:none;"
+            session['hide'] = ""
+            return redirect('/',)
+        if session['guess'] > session['number']:
+            session['result'] = "Too High!"
+            session['style'] = "toohigh"
+            print(session['result'])
+            return redirect('/')
+        if session['guess'] < session['number']:
+            session['result'] = "Too Low!"
+            session['style'] = "toolow"
+            print(session['result'])
+        return redirect('/')
+    else:
+        session['result'] = "You didn't enter a whole integer!"
+        return redirect('/')
+
+@app.route('/reset',methods=['POST'])
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+app.run(debug=True)
